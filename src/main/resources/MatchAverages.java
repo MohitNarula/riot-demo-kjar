3265808c04294e21b6206011fc6e37ce56dea008
wbadmin <>
2018-09-20 10:27:13 -0700

    {/src/main/resources/MatchAverages.java}

---
+++ src/main/resources/MatchAverages.java
@@ -0,0 +1,16 @@
+package com.myapp;
+
+public class MatchAverages {
+    private int avgBaronKills;
+    private int avgDragonKills;
+    private int percentFirstBaron;
+    private int percentFirstDragon;
+    private int percentFirstInhibitor;
+    private int percentFirstHerald;
+    private int avgInhibitorKills;
+    private int avgHeraldKills;
+    private int percentWin;
+    
+    
+    
+}
